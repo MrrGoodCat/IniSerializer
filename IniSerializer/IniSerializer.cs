a7126d9d964b790bb7cb328acb8588b2282ac426
MrrGoodCat <mrrgoodcat@gmail.com>
2016-07-01 19:22:44 +0300

    New

---
+++ IniSerializer/IniSerializer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace IniSerializer
+{
+    public class IniSerializer : XmlSerializer
+    {
+        string iniFilePath = null;
+
+        XmlSerializer serializer;
+
+        public string IniFilePath
+        {
+            get
+            {
+                return iniFilePath;
+            }
+
+            set
+            {
+                iniFilePath = value;
+            }
+        }
+
+        public IniSerializer()
+        {
+            serializer = new XmlSerializer(iniFilePath,);
+        }
+    }
+}
